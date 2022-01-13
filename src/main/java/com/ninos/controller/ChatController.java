628bedd8c04858d78e961f8b5587f60cf79d8464
NinoosMoshi <nalkaton@gmail.com>
2022-01-13 17:41:52 -0500

    create controller

---
+++ src/main/java/com/ninos/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.ninos.controller;
+
+import com.ninos.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.login")
+    @SendTo("/topic/all")
+    public ChatMessage login(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
+        return chatMessage;
+    }
+
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/all")
+    public ChatMessage send(@Payload ChatMessage chatMessage){
+      return chatMessage;
+    }
+
+
+
+
+}
