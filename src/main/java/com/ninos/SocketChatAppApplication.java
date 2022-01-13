3ee392ef891ec97207db738b9f0574d0079db693
NinoosMoshi <nalkaton@gmail.com>
2022-01-13 09:12:32 -0500

    init project

---
+++ src/main/java/com/ninos/SocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketChatAppApplication.class, args);
+	}
+
+}
