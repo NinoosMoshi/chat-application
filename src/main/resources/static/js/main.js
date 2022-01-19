d65d5ae115e8b6dd16ff9f00cec2e53302f0fde5
NinoosMoshi <nalkaton@gmail.com>
2022-01-18 20:01:56 -0500

    add url

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,13 +3,14 @@ var chatElement = document.querySelector('#chat');
 var userForm = document.querySelector('#userForm');
 var userName= null;
 var stomp = null;
+var URL = "http://localhost:8080"
 
 function connectSocket(event){
     userName = document.querySelector('#username').value.trim();  // get the value of id=username
     if (userName){   // if there is a value in userName
         loginElement.classList.add('dis'); // put dis class inside this element(dis class use to disappear)
         chatElement.classList.remove('dis');  // remove class dis from chatElement
-        var socket = new SockJS("/connect");   // connect path is from backEnd to open a connection between client and backend
+        var socket = new SockJS(URL + '/connect');   // connect path is from backEnd to open a connection between client and backend
         stomp = Stomp.over(socket); // make connection via socket
         stomp.connect({},connectedDone);
     }
@@ -18,8 +19,8 @@ function connectSocket(event){
 
 
 function connectedDone(){
-    stomp.subscribe("/app/chat.send",sendMessage)
     stomp.send("/app/chat.login",{},JSON.stringify({sender:userName, chatType:'JOIN'}));
+    stomp.subscribe("/app/chat.send",sendMessage);
 }
 
 function sendMessage(){
